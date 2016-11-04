f524e02675b9f73eb916f3b4b4c4b121bdc80162
Grabin, Paulo <paulo.grabin@sap.com>
2016-11-04 12:13:20 -0200

    Converting byte to String before output. Adding comments to help debugging.

--- src/main/java/com/paulograbin/brainfuck/commands/OutputByteCommand.java
+++ src/main/java/com/paulograbin/brainfuck/commands/OutputByteCommand.java
@@ -7,7 +7,13 @@ public class OutputByteCommand implements Command {
 
     @Override
     public State execute(State programState) {
-        System.out.println(programState.getCurrentValue());
+        byte currentValue[] = new byte[1];
+        currentValue[0] = programState.getCurrentValue();
+        String convertedOutput = new String(currentValue);
+
+        System.out.println("Output value " + currentValue[0] + " converted to " + convertedOutput);
+
+        System.out.println(convertedOutput);
 
         return programState;
     }
