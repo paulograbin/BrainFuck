ce82aa231443f8f3ee54a577969e0979a88068c9
Paulo Grabin <plgrabin@gmail.com>
2026-04-03 18:20:20 -0300

    Extract program from machine

--- src/main/java/com/paulograbin/brainfuck/Program.java
+++ src/main/java/com/paulograbin/brainfuck/Program.java
@@ -1,95 +1,36 @@
 package com.paulograbin.brainfuck;
 
-import java.util.Scanner;
-
-
 public class Program {
 
-    public static final char RIGHT = '>';
-    public static final char LEFT = '<';
-    public static final char PLUS = '+';
-    public static final char MINUS = '-';
-    public static final char OUTPUT = '.';
-    public static final char INPUT = ',';
-    public static final char BRANCH_START = '[';
-    public static final char BRANCH_END = ']';
-
     private final String sourceCode;
-    private final byte[] memory;
-    private int commandCounter;
-    private int memoryPointer;
+    private StringBuilder output;
 
     public Program(String sourceCode) {
         this.sourceCode = sourceCode;
-
-        this.memory = new byte[30000];
-        this.memoryPointer = 0;
+        this.output = new StringBuilder();
     }
 
-    private void execute() {
-        char currentCommand;
-
-        while(commandCounter < sourceCode.length()) {
-            currentCommand = sourceCode.charAt(commandCounter);
-
-            if (currentCommand == PLUS) {
-                memory[memoryPointer]++;
-                commandCounter++;
-            } else if (currentCommand == MINUS) {
-                memory[memoryPointer]--;
-                commandCounter++;
-            } else if (currentCommand == RIGHT) {
-                memoryPointer += 1;
-                commandCounter++;
-            }
-            else if (currentCommand == LEFT) {
-                memoryPointer -= 1;
-                commandCounter++;
-            }
-            else if (currentCommand == OUTPUT) {
-                byte currentValue[] = new byte[1];
-                currentValue[0] = memory[memoryPointer];
-
-                System.out.print(new String(currentValue));
-
-                commandCounter++;
-            }
-            else if (currentCommand == INPUT) {
-                Scanner s = new Scanner(System.in);
-                System.out.print("Input value: ");
-                String input = s.nextLine();
-
-                memory[memoryPointer] = input.getBytes()[0];
-            }
-            else if (currentCommand == BRANCH_START) {
-                if(memory[memoryPointer] == 0) {
-                    while(sourceCode.charAt(commandCounter) != ']') {
-                        commandCounter += 1;
-                    }
-                } else {
-                    commandCounter++;
-                }
-            }
-            else if (currentCommand == BRANCH_END) {
-                if(memory[memoryPointer] == 0) {
-                    commandCounter++;
-                }
-                else {
-                    while(sourceCode.charAt(commandCounter) != '[') {
-                        commandCounter -= 1;
-                    }
-                }
-            }
-        }
+    public void appendToOutput(byte b) {
+        output.append((char) b);
     }
 
+    public void appendToOutput(char c) {
+        output.append(c);
+    }
 
+    public void appendToOutput(String s) {
+        output.append(s);
+    }
 
-    public static void main(String... args) {
-        String sourceCode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+    public StringBuilder getOutput() {
+        return output;
+    }
 
-        Program aSimpleProgram = new Program(sourceCode);
+    public void setOutput(StringBuilder output) {
+        this.output = output;
+    }
 
-        aSimpleProgram.execute();
+    public String getSourceCode() {
+        return sourceCode;
     }
 }
